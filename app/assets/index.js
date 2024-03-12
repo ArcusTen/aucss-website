cafacd7ff04d3707d41b75be562d41e9eb9466f0
Air University Cyber Security Society <157748144+aucssociety@users.noreply.github.com>
2024-03-12 19:22:35 +0500

    added files

---
+++ app/assets/index.js
@@ -0,0 +1,166 @@
+import aucss from '@/app/assets/logos/AUCSS-Logo.png';
+import au from '@/app/assets/logos/AU-Logo.png';
+import cys from '@/app/assets/logos/CYS-Logo.png';
+import recypherlogo from '@/app/assets/logos/recypher.png';
+import erasmus from '@/app/assets/logos/erasmus.jpg';
+import hero from '@/app/assets/hero-img.gif'
+import about from '@/app/assets/about-us.jpeg'
+import awarenessclub from "@/app/assets/clubs/awareness-club.png";
+import red_teaming from "@/app/assets/clubs/red_teaming.png";
+import blue_teaming from "@/app/assets/clubs/blue_teaming.png";
+import ctf_team from "@/app/assets/clubs/ctf_team.png";
+import developers from '@/app/assets/clubs/developers-club.png'
+import creative from "@/app/assets/clubs/creative-club.png";
+import protocol from "app/assets/clubs/security-team.png";
+import public_relations from '@/app/assets/clubs/public_relations.png'
+import sportsMale from "@/app/assets/clubs/sports-club(male).png";
+import socialmedia from "@/app/assets/clubs/socialmedia-club.png";
+import photography from "@/app/assets/clubs/photography-club.png";
+import sportsFemale from "@/app/assets/clubs/sports-club(female).png";
+import aucsc from "@/app/assets/clubs/au-cyberskills-club.png";
+import women from "app/assets/clubs/women-club.png";
+import kashif from "app/assets/execs/Kashif-modified.png";
+import saif from "app/assets/execs/saif.png";
+import maila from "app/assets/execs/maila.png";
+import abdullah from "app/assets/execs/abdullah.jpg";
+import husnain from "app/assets/execs/hussnain.jpg";
+import hamzah from "app/assets/execs/hamza.jpg";
+import ahmadali from "app/assets/execs/ahmad-ali.jpg";
+import basma from "app/assets/execs/basma.jpg";
+import haram from "app/assets/execs/haram.jpg";
+import rafay from "app/assets/execs/rafay.jpg";
+import isqtc from "app/assets/cluster/isqtc.png";
+import uetp from "app/assets/cluster/uetp.png";
+import cg from "app/assets/cluster/cg.png";
+import uet from "app/assets/cluster/uet.png";
+import ueit from "app/assets/cluster/ueit.png";
+import sheclub from "app/assets/cluster/sheclub.png";
+import cyseckarachi from "app/assets/cluster/cyseckarachi.png";
+import fucsc from "app/assets/cluster/fucsc.png";
+import csc from "app/assets/cluster/csc.png";
+import cyberquest from "app/assets/cluster/cyberquest.png";
+import abdulwali from "app/assets/cluster/abdulwali.png";
+import ist from "app/assets/cluster/ist.png";
+import security from "app/assets/teams/security.jpg";
+import management from "app/assets/teams/management.jpg";
+import technical from "app/assets/teams/technical.jpg";
+import awareness from "@/app/assets/teams/awareness.jpg";
+import graphics from "@/app/assets/teams/graphics.jpg";
+import media from "app/assets/teams/media-team.png";
+import humanFactors from "app/assets/events/human-conference.jpeg";
+import iccws from "app/assets/events/iccws2022.jpg";
+import hackathon from "app/assets/events/hackathon-training-event.png";
+import cybersiege from "app/assets/events/cybersiege-event.png";
+import fypseminar from "app/assets/events/fyp-seminar-event.png";
+import cybersiegeWorkshop from "app/assets/events/cybersiege-workshop-event.png";
+import chescrypt from "app/assets/events/chescrypt-event.png";
+import connectTech from "app/assets/events/connect-tech-event.png";
+import cyberstorm from "app/assets/events/cyberstorm-event.png";
+import cyberFundamentals from "app/assets/events/cyber-fundamentals-event.png";
+import futsal from "app/assets/events/futsal-match-event.png";
+import digitalArt from "app/assets/events/digital-art-event.png";
+import etizazTalk from "app/assets/events/etizaz-mohsin-event.png";
+import webSecurity from "app/assets/events/web-security-event.png";
+import careerWebinar from "app/assets/events/career-webinar-event.png";
+import farewell from "app/assets/events/farewell-event.png";
+import recypher from "app/assets/events/recypher-workshop-event.png";
+import contactlogo from "app/assets/inky-safety-1.png";
+import introductory from "app/assets/events/introductory-event.jpeg";
+import cyberstorm2 from "app/assets/events/cyberstorm-2-event.jpeg";
+import crackingCode from "app/assets/events/cracking_the_code.jpg";
+import cybHer from "app/assets/events/cybHer.png";
+import osint101 from "app/assets/events/osint_101.png";
+import blueHorizon from "app/assets/events/blue_horizon.png";
+import cispa from "app/assets/events/cispa-2023.jpeg";
+import welcome from "app/assets/events/welcome-event.jpeg";
+import khawaja from "app/assets/execs/sir_khawaja.jpg";
+import umair from "app/assets/execs/umair-modified.png";
+import qamar from "app/assets/execs/qamar-modified.png";
+import fiza from "app/assets/execs/fiza-modified.png";
+import bugBountyTalk from "app/assets/events/bug-bounty-workshop-event.jpg";
+import projectManagementCourse from "app/assets/events/project-management-course.jpg";
+
+
+export {
+  aucss,
+  au,
+  cys,
+  recypherlogo,
+  erasmus,
+  kashif,
+  hero,
+  about,
+  developers,
+  creative,
+  awareness,
+  red_teaming,
+  blue_teaming,
+  ctf_team,
+  awarenessclub,
+  protocol,
+  public_relations,
+  sportsMale,
+  socialmedia,
+  photography,
+  sportsFemale,
+  women,
+  aucsc,
+  saif,
+  maila,
+  husnain,
+  hamzah,
+  abdullah,
+  ahmadali,
+  basma,
+  haram,
+  rafay,
+  isqtc,
+  uetp,
+  cg,
+  uet,
+  ueit,
+  sheclub,
+  cyseckarachi,
+  fucsc,
+  csc,
+  cyberquest,
+  abdulwali,
+  ist,
+  security,
+  management,
+  technical,
+  graphics,
+  media,
+  humanFactors,
+  iccws,
+  hackathon,
+  cybersiege,
+  fypseminar,
+  cybersiegeWorkshop,
+  chescrypt,
+  connectTech,
+  cyberstorm,
+  cyberFundamentals,
+  futsal,
+  digitalArt,
+  etizazTalk,
+  webSecurity,
+  bugBountyTalk,
+  projectManagementCourse,
+  careerWebinar,
+  farewell,
+  recypher,
+  contactlogo,
+  introductory,
+  cyberstorm2,
+  crackingCode,
+  cybHer,
+  osint101,
+  blueHorizon,
+  cispa,
+  welcome,
+  khawaja,
+  umair,
+  qamar,
+  fiza
+};
