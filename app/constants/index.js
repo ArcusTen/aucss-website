77141c01fee90132d82fa11e27f8b976e753d76b
Air University Cyber Security Society <157748144+aucssociety@users.noreply.github.com>
2024-03-12 19:16:44 +0500

    Add files via upload

---
+++ app/constants/index.js
@@ -0,0 +1,26 @@
+export const navLinks = [
+  {
+    id: "",
+    title: "INTRODUCTION",
+  },
+  {
+    id: "/#about",
+    title: "ABOUT US",
+  },
+  {
+    id: "/#history",
+    title: "ORGANIZATION",
+  },
+  {
+    id: "/#events",
+    title: "EVENTS",
+  },
+  {
+    id: "/#airange",
+    title: "AIRANGE",
+  },
+  {
+    id: "/#contact",
+    title: "CONTACT US",
+  },
+];
