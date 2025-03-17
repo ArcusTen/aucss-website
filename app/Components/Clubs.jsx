b74266d6924cfebba9689d84307e007384163387
kumail <231351@students.au.edu.pk>
2025-03-17 23:23:58 +0500

    leads jango

--- app/Components/Clubs.jsx
+++ app/Components/Clubs.jsx
@@ -1,38 +1,42 @@
 import React from 'react'
 import Image from 'next/image';
-import { decor, operations, creative, protocol, red_teaming, ctf_team, blue_teaming, awarenessclub, sportsMale, sportsFemale, women, photography, aucsc, entertainment, graphicsteam  } from '../assets';
+import { decor, operations, creative, protocol, red_teaming, ctf_team, blue_teaming, awarenessclub, sportsMale, sportsFemale, women, photography, aucsc, entertainment, graphicsteam } from '../assets';
 
 const clubs = [
   {
     name: "Red Teaming",
     leadName: "Lead: Abdul Wahab Khan",
-    coLeadName: "Co-Lead: Mishqat Abid",
+    coLeadName: ["Co-Lead: Mishqat Abid"],
     imageUrl: red_teaming,
   },
   {
     name: "Blue Teaming",
     leadName: "Lead: Muzammil Akhtar",
-    coLeadName: "Co-Lead: Hanzalah",
+    coLeadName: ["Co-Lead: Hanzalah"],
     imageUrl: blue_teaming,
   },
   {
     name: "CTF Team",
     leadName: "CTF QA Lead: Radeel Ahmed",
-    coLeadName: "CTF Front-End Lead: Muhammad Mazhar",
-    coLeadName: "CTF Back-End Lead: Khizar Shah",
+    coLeadName: [
+      "CTF Front-End Lead: Muhammad Mazhar",
+      "CTF Back-End Lead: Khizar Shah"
+    ],
     imageUrl: ctf_team,
   },
   {
     name: "Security and Protocol Team",
     leadName: "Leads: Muhammad Abdullah Tarar",
-    coLeadName: "Co-Lead: Fatima",
+    coLeadName: ["Co-Lead: Fatima"],
     imageUrl: protocol,
   },
   {
     name: "Media Team",
     leadName: "Lead: Hammad Ahmed",
-    coLeadName: "Co-Lead: Fizza Afzal",
-    coLeadName: "Co-Lead Coverage: Mikail Afridi",
+    coLeadName: [
+      "Co-Lead: Fizza Afzal",
+      "Co-Lead Coverage: Mikail Afridi"
+    ],
     imageUrl: photography,
   },
   {
@@ -40,65 +44,66 @@ const clubs = [
     leadName: "Lead: Tooba Arshad",
     coLeadName: [
       "Co-Lead Management: Zeeshan Ali",
-      "Co-Lead Management: Zeeshan Ali",
+      "Co-Lead Operations: Sardar Ahmed"
     ],
     imageUrl: operations,
   },
   {
     name: "Graphics Team",
     leadName: "Lead: Bareeha",
-    coLeadName: "Co-Lead: Iqra Noor",
+    coLeadName: ["Co-Lead: Iqra Noor"],
     imageUrl: graphicsteam,
   },
   {
     name: "Awareness Club",
     leadName: "Lead: Nahl Imran",
-    coLeadName: "Co-Lead: Eman Naseer",
+    coLeadName: ["Co-Lead: Eman Naseer"],
     imageUrl: awarenessclub,
   },
   {
     name: "Cyber Skills Club",
     leadName: "Lead: Waqar",
-    coLeadName: "Co-Lead: Hira Asmat",
+    coLeadName: ["Co-Lead: Hira Asmat"],
     imageUrl: aucsc,
   },
   {
     name: "Research Club",
     leadName: "Lead: Effa",
-    coLeadName: "Co-Lead: Junaid",
+    coLeadName: ["Co-Lead: Junaid"],
     imageUrl: creative,
   },
   {
     name: "Women in Cyber Security Club",
     leadName: "Lead: Zuha Fatima",
-    coLeadName: "Co-Lead: Sommia Jalil",
+    coLeadName: ["Co-Lead: Sommia Jalil"],
     imageUrl: women,
   },
   {
     name: "Sports Club (Female)",
     leadName: "Lead: Zujaj Shafaq",
-    coLeadName: "Co-Lead: Alizeh",
+    coLeadName: ["Co-Lead: Alizeh"],
     imageUrl: sportsFemale,
   },
   {
     name: "Sports Club (Male)",
     leadName: "Lead: Taha Qamar",
-    coLeadName: "Co-Lead: Talal Ahmed",
+    coLeadName: ["Co-Lead: Talal Ahmed"],
     imageUrl: sportsMale,
   },
   {
     name: "Entertainment Club",
     leadName: "Lead: Ibad ul Haq",
-    coLeadName: "Co-Lead: Waqar",
+    coLeadName: ["Co-Lead: Waqar"],
     imageUrl: entertainment,
   },
   {
     name: "Decor Club",
     leadName: "Lead: Sadia Naveed",
-    coLeadName: "Co-Lead: Zeeshan Ahmed",
+    coLeadName: ["Co-Lead: Zeeshan Ahmed"],
     imageUrl: decor,
   },
 ];
+
 const Clubs = () => {
   return (
     <section className="bg-white py-24 sm:py-32 font-montserrat" id="clubs">
@@ -108,8 +113,7 @@ const Clubs = () => {
             Our Clubs
           </h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Our society consists of 8 clubs, each of which is led by a club
-            lead.
+            Our society consists of 8 clubs, each of which is led by a club lead.
           </p>
         </div>
         <ul
@@ -121,7 +125,7 @@ const Clubs = () => {
               <Image
                 className="aspect-[2/2] w-full rounded-2xl object-cover"
                 src={club.imageUrl}
-                alt=""
+                alt={club.name}
                 width={500}
                 height={500}
               />
@@ -129,7 +133,9 @@ const Clubs = () => {
                 {club.name}
               </h3>
               <p className="text-base leading-7 text-gray-600">{club.leadName}</p>
-              <p className="text-base leading-7 text-gray-600">{club.coLeadName}</p>
+              {club.coLeadName.map((coLead, index) => (
+                <p key={index} className="text-base leading-7 text-gray-600">{coLead}</p>
+              ))}
             </li>
           ))}
         </ul>
@@ -138,4 +144,4 @@ const Clubs = () => {
   );
 }
 
-export default Clubs
+export default Clubs;
